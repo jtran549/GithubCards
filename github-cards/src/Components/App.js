43749e948608ccf2ac5c602f81b850f082b9b8bb
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-07-27 21:02:00 -0400

    Created form component, changed location of testdata

--- github-cards/src/Components/App.js
+++ github-cards/src/Components/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Card from './Card';
 import CardList from './CardList';
+import Form from './Form';
 import '../css/App.css';
+
+const testData = [
+  {name: "Jimmy Tran", company: "Facebook"},
+  {name: "DJ Herier", company: "Speedway"},
+  {name: "David Williams", company: "KMK Law"}    
+]
 
 class App extends React.Component {
   render () {
     return (
       <div className="App-header">
         {this.props.title}
-        <CardList />
+        <Form />
+        <CardList profiles={testData}/>
       </div>
     );
   }
--- github-cards/src/Components/CardList.js
+++ github-cards/src/Components/CardList.js
@@ -1,15 +1,10 @@
 import React from "react";
 import Card from "./Card";
-const testData = [
-    {name: "Jimmy Tran", company: "Facebook"},
-    {name: "DJ Herier", company: "Speedway"},
-    {name: "David Williams", company: "KMK Law"}    
-]
 
-const CardList = () => {
+const CardList = (props) => {
     return(
         <div>
-            {testData.map(profile => <Card {...profile}/>)}
+            {props.profiles.map(profile => <Card {...profile}/>)}
         </div>
     )
 }
